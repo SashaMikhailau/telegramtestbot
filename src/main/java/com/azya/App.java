217fec85169dfd09c12a284cf35f38ba0626d153
Aliaksandr Mikhailau <aliaksandr_mikhailau2@epam.com>
2019-07-17 14:37:03 +0300

    Commit from gui

--- src/main/java/com/azya/App.java
+++ src/main/java/com/azya/App.java
@@ -19,6 +19,7 @@ public class App
         } catch (TelegramApiRequestException e) {
             e.printStackTrace();
             System.out.println("123");
+            System.out.println("123");
         }
 
     }
